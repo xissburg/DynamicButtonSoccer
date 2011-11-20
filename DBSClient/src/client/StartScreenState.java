a5a8b2d79f9c6f26f00f79e46d7aed63b2250f43
xissburg <xissburg@gmail.com>
2011-11-20 21:08:17 -0200

    Basic login form.

--- DBSClient/src/client/StartScreenState.java
+++ DBSClient/src/client/StartScreenState.java
@@ -7,15 +7,19 @@ import com.jme3.app.state.AppStateManager;
 import com.jme3.niftygui.NiftyJmeDisplay;
 import com.jme3.scene.Node;
 import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.controls.TextField;
+import de.lessvoid.nifty.screen.Screen;
+import de.lessvoid.nifty.screen.ScreenController;
 
 /**
  * AppState for the title screen with a simple gui.
  * @author xissburg
  */
-public class StartScreenState extends AbstractAppState {
+public class StartScreenState extends AbstractAppState implements ScreenController {
 
     private Node rootNode;
     private ClientApplication app;
+    Nifty nifty;
 
     public StartScreenState(Node rootNode) {
         this.rootNode = rootNode;
@@ -32,13 +36,49 @@ public class StartScreenState extends AbstractAppState {
         //Add gui to the screen
         NiftyJmeDisplay niftyDisplay = new NiftyJmeDisplay(this.app.getAssetManager(), 
                 this.app.getInputManager(), this.app.getAudioRenderer(), this.app.getGuiViewPort());
-        Nifty nifty = niftyDisplay.getNifty();
-        nifty.fromXml("Interfaces/TitleScreen.xml", "start");
+        Nifty n = niftyDisplay.getNifty();
+        n.fromXml("Interfaces/TitleScreen.xml", "start", this);
         this.app.getGuiViewPort().addProcessor(niftyDisplay);
         
         //Setup event handlers
         
     }
+
+    /**
+     * ScreenController
+     */
+    @Override
+    public void bind(Nifty nifty, Screen screen) {
+        this.nifty = nifty;
+    }
+
+    @Override
+    public void onStartScreen() {
+        
+    }
+
+    @Override
+    public void onEndScreen() {
+        
+    }
     
+    /**
+     * Screen Events
+     */
+    public void login() {
+        String username = nifty.getCurrentScreen().
+                findNiftyControl("UserNameTextField", TextField.class).getText();
+        String password = nifty.getCurrentScreen().
+                findNiftyControl("PasswordTextField", TextField.class).getText();
+        String url = nifty.getCurrentScreen().
+                findNiftyControl("URLTextField", TextField.class).getText();
+        String port = nifty.getCurrentScreen().
+                findNiftyControl("PortTextField", TextField.class).getText();
+        System.out.println(String.format("Username: %s\nPassword: %s\nURL: %s\nPort: %s\n", 
+                username, password, url, port));
+    }
     
+    public void quit() {
+        app.stop();
+    }
 }
